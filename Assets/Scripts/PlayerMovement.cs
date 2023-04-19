34ed4fe5ad4814e41e7475ada769aa11120683c1
kuon9 <35052089+kuon9@users.noreply.github.com>
2023-04-19 10:26:15 -0700

    Horizontal Movement
    Added Horizontal movement for character

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public GameObject player;
+    
+    [Header("PlayerAttributes")]
+    [SerializeField] float playerSpeed = 10f;
+    [SerializeField] float jumpSpeed = 5f;
+    [SerializeField] float climbSpeed = 5f;
+
+    Vector2 MovementInput;
+
+    public bool isAlive = true;
+
+
+    Animator anim;
+    Rigidbody2D rigidbody;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        rigidbody = GetComponent<Rigidbody2D>();
+            
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!isAlive) {return;}
+        {
+            Run();    
+        }    
+    }
+
+    void OnMove(InputValue input)
+    {
+        if(!isAlive) {return;}
+        MovementInput = input.Get<Vector2>();
+    }
+
+    void Run()
+    {
+        Vector2 playervelocity = new Vector2(MovementInput.x * playerSpeed, rigidbody.velocity.y);
+        rigidbody.velocity = playervelocity;
+
+        bool playerHasHorizontalSpeed = Mathf.Abs(rigidbody.velocity.x) > Mathf.Epsilon;
+        anim.SetBool("IsRunning", playerHasHorizontalSpeed);
+    }
+
+}

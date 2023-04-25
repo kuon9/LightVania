602c0af5a617a70fdcafe9eb189196099ad5bd14
kuon9 <35052089+kuon9@users.noreply.github.com>
2023-04-24 18:13:40 -0700

    Checkpoints
    Added checkpoints script for player to respawn

---
+++ Assets/Scripts/CheckPoint.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckPoint : MonoBehaviour
+{
+    [SerializeField] AudioClip chcekPointSFX;
+    AudioSource audioSource;
+
+    private bool isChecked;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        isChecked = false;    
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Player" && !isChecked)
+        {
+            //GameSession.playerLives++;
+            Debug.Log("New Checkpoint acquired");
+            audioSource.PlayOneShot(chcekPointSFX);
+            isChecked = true;
+            //FindObjectOfType<PlayerMovement>().Checkpoint = transform.position;
+        }
+    }
+}
